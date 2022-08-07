edeac62ee0c39f732aa2c9e82a2b385d373faf10
akash121941 <110765513+akash121941@users.noreply.github.com>
2022-08-07 16:07:46 +0530

    Add files via upload

---
+++ AKASHFLIX-main/template-youflix-master/main.js
@@ -0,0 +1,31 @@
+// Wait for page to load
+document.addEventListener('DOMContentLoaded', function(event) {
+	ready();
+})
+
+function ready() {
+	let url = 'https://api.sheety.co/6632ef44f02ae244c9112d40731780f3/akashflix/videos';
+	fetch(url)
+	.then((response) => response.json())
+	.then(json => {
+		let videos = json.videos;
+		// Parse out YouTube ID from the URL, as we'll use that to get the thumbnail
+		videos.forEach(video => {
+			let regex = /watch\?v=([\w\d-]*)/;
+			video.youtubeID = video.url.match(regex)[1];
+		});
+		
+		let featuredVideo = videos.find(video => {
+			return video.isFeatured;
+		});
+	
+		let groupedVideos = _.groupBy(json.videos, 'category');
+		var template = Handlebars.compile(document.getElementById("app-template").innerHTML);
+		// Render items into Handlebars template, and set the html of the container element
+		document.getElementById('app').innerHTML = template({
+			featured: featuredVideo,
+			videos: groupedVideos
+		});
+	});
+}
+
